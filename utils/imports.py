9dd05831079b1963e8450e147688d144e59a68f8
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2026-04-01 20:58:27 +0200

    Update imports.py

--- utils/imports.py
+++ utils/imports.py
@@ -6,7 +6,6 @@ from discord.ui import View, Button, Select
 from discord.utils import format_dt
 from discord import Activity, ActivityType
 import os
-from dotenv import load_dotenv
 import datetime
 from datetime import datetime, timezone, timedelta
 import psutil
@@ -23,6 +22,10 @@ import random
 import traceback
 from io import BytesIO
 import io
+import json
+import logging
+from typing import Optional, Union
+
 
 from discord import (
     ApplicationContext,
@@ -43,31 +46,17 @@ from discord.ui import (
     Separator,
     TextDisplay,
     Thumbnail,
-    button,
 )
-
-load_dotenv()
-
-TOKEN = os.getenv("TOKEN")
-OWNER = [int(user) for user in os.getenv("OWNER", "").split(",") if user.strip()]
-GUILDS = [int(guild) for guild in os.getenv("GUILDS", "").split(",") if guild.strip()]
-MOD_ROLE_IDS = [int(x) for x in os.getenv("MOD_ROLE_IDS", "").split(",") if x]
-ADMIN_ROLE_IDS = [int(x) for x in os.getenv("ADMIN_ROLE_IDS", "").split(",") if x]
-FORUM_ID = int(os.getenv("FORUM_ID"))
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-ERROREMOJI = os.getenv("ERROREMOJI")
-ERRORCHANNEL = os.getenv("ERRORCHANNEL")
-SUPPORT_SERVER = os.getenv("SUPPORT_SERVER")
 
 __all__ = [
     'BytesIO', 'discord', 'commands', 'slash_command', 'SlashCommandGroup',
     'Option', 'OptionChoice', 'View', 'Button', 'Select', 'format_dt',
-    'Activity', 'ActivityType', 'os', 'load_dotenv', 'datetime', 'timezone',
+    'Activity', 'ActivityType', 'os', 'datetime', 'timezone',
     'timedelta', 'psutil', 'platform', 'time', 're', 'requests', 'aiohttp',
     'hashlib', 'asyncio', 'sys', 'sqlite3', 'random', 'traceback',
     'ApplicationContext', 'ButtonStyle', 'Color', 'File', 'Interaction',
     'SeparatorSpacingSize', 'User', 'ActionRow', 'UIButton', 'Container',
     'DesignerView', 'MediaGallery', 'Section', 'Separator', 'TextDisplay',
-    'Thumbnail', 'button', 'io',
-    'TOKEN', 'OWNER', 'GUILDS', 'MOD_ROLE_IDS', 'ADMIN_ROLE_IDS', 'FORUM_ID', 'WEBHOOK_URL', 'ERROREMOJI', 'SUPPORT_SERVER',
+    'Thumbnail', 'io', "json", "logging", "Optional", "Union",
+
 ]
